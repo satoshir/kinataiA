e8ce6fcf58ae4bb04ed2693495cb71b69463b0f9
satoshi3 <satoshi@email.com>
2022-06-10 12:56:38 +0000

    6.5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+  @user = User.new(user_params)
     if @user.save
       log_in @user # 保存成功後、ログインします。
       flash[:success] = '新規作成に成功しました。'
