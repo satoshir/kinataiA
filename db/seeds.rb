641c9f26af9e6cb8537d61896e5a171c018db176
satoshi3 <satoshi@email.com>
2022-06-27 12:08:36 +0000

    上長完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,27 @@ User.create!(name: "Sample User",
              password: "password",
              password_confirmation: "password",
              admin: true)
+             
+@testuser = User.create!(name: "上長A",
+             email: "sample-a@email.com",
+             password: "password",
+             password_confirmation: "password",
+             admin: false,
+             superior: true)
+if @testuser
+    puts "Superior1 User Sucess."
+end
+
+@testuser = User.create!(name: "上長B",
+             email: "sample-b@email.com",
+             password: "password",
+             password_confirmation: "password",
+             admin: false,
+             superior: true)
+if @testuser
+    puts "Superior2 User Sucess."
+end 
+
 
 60.times do |n|
   name  = Faker::Name.name
@@ -15,4 +36,6 @@ User.create!(name: "Sample User",
                password: password,
                password_confirmation: password)
 end
+
+
 
