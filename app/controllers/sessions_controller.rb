d5b197337308a0b17b5cb83aeacfc451844ed0a4
satoshi3 <satoshi@email.com>
2022-06-28 12:49:13 +0000

    残業申請作業前

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,12 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      if current_user.admin?
+        redirect_to root_url
+      else
+        redirect_back_or user
+      end
+      # redirect_back_or user
     else
       flash.now[:danger] = '認証に失敗しました。'
       render :new
